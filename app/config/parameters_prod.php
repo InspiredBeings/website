5aa0713ef1c93005b9531c2f7378c058761126eb
Ivan Gabriele <ivan.gabriele@gmail.com>
2016-07-04 08:27:50 +0200

    Dev: Work in progress

--- app/config/parameters_prod.php
+++ app/config/parameters_prod.php
@@ -4,8 +4,8 @@ $jawsDb = parse_url(getenv('JAWSDB_MARIA_URL'));
 
 $container->setParameter('database_driver', 'pdo_mysql');
 $container->setParameter('database_host', $jawsDb['host']);
-$container->setParameter('database_name', ltrim($jawsDb["path"], '/'));
+$container->setParameter('database_name', substr($jawsDb["path"], 1));
 $container->setParameter('database_password', $jawsDb['pass']);
-$container->setParameter('database_port', null);
+$container->setParameter('database_port', 3306);
 $container->setParameter('database_user', $jawsDb['user']);
 $container->setParameter('secret', getenv('SYMFONY_SECRET'));
