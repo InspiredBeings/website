1b15907ad1f9db1a7a7466397342b47522f8d798
Ivan Gabriele <ivan.gabriele@gmail.com>
2016-07-04 10:00:59 +0200

    Dev: Work in progress

--- test.php
+++ test.php
@@ -1,7 +1,23 @@
 <?php
 
 $jawsDb = parse_url(getenv('JAWSDB_MARIA_URL'));
-echo $jawsDb['host'] . "\n";
-echo $jawsDb['user'] . "\n";
-echo $jawsDb['pass'] . "\n";
-echo substr($jawsDb["path"], 1);
+$hostname = $jawsDb['host'];
+$username = $jawsDb['user'];
+$password = $jawsDb['pass'];
+$database = substr($jawsDb["path"], 1);
+
+echo $hostname . "\n";
+echo $username . "\n";
+echo $password . "\n";
+echo $database . "\n";
+
+try {
+    $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
