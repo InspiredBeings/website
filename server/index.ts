586e3e37454f34a07bfb3f827d7c8fe33e9cf509
Ivan Gabriele <ivan.gabriele@gmail.com>
2017-10-15 03:13:34 +0200

    Dev: Work in progress

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,15 @@
 import { Lexpress } from 'lexpress'
+import * as fs from 'fs'
+
 
 import routes from './routes'
 
 const lexpress = new Lexpress({
-  routes
+  https: {
+    cert: fs.readFileSync('../server.crt', 'utf8'),
+    key: fs.readFileSync('../server.key', 'utf8'),
+  },
+  routes,
 })
 
 lexpress.start()
